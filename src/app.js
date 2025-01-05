7225ba9535dc2b3329ab830bbe7b7adfa58e9de1
Davidgmzc <davidgomz.c13@gmail.com>
2025-01-05 14:20:39 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const listaDeValores1 = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "J",
+  "Q",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const listaDeTipos1 = ["♠️", "♥️", "♦️", "♣️"];
+
+const getValorAleatorio = () => {
+  let random = Math.random();
+  let item = Math.floor(random * listaDeValores1.length);
+  return listaDeValores1[item];
 };
+const getTipoAleatorio = () => {
+  let random2 = Math.random();
+  let item2 = Math.floor(random2 * listaDeTipos1.length);
+  return listaDeTipos1[item2];
+};
+const setValorAleatorio = valor => {
+  document.querySelector("#valor1").innerHTML = valor;
+};
+const setTipoAleatorio = tipo => {
+  document.querySelector("#tipo1").innerHTML = tipo;
+  document.querySelector("#segundo-Tipo").innerHTML = tipo;
+};
+
+const generaUnaCartaAleatoria = () => {
+  const valor = getValorAleatorio();
+  const tipo = getTipoAleatorio();
+  setValorAleatorio(valor);
+  setTipoAleatorio(tipo);
+};
+window.generaUnaCartaAleatoria = generaUnaCartaAleatoria;
